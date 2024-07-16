e7443117048d4eb5385b444deb9ee311b263f964
Miroslav Koula <miroslav.koula@seyvillas.com>
2024-07-16 10:45:36 +0200

    - Added `track_total_hits` functionality

--- src/Builder.php
+++ src/Builder.php
@@ -33,6 +33,8 @@ class Builder
 
     protected bool $withAggregations = true;
 
+    protected bool $trackTotalHits = false;
+
     protected ?array $scripts = null;
 
     public function __construct(protected Client $client)
@@ -92,12 +94,23 @@ class Builder
             $params['from'] = $this->from;
         }
 
+        if($this->trackTotalHits) {
+            $params['track_total_hits'] = true;
+        }
+
         return $this->client->search($params);
     }
 
     public function index(string $searchIndex): static
     {
         $this->searchIndex = $searchIndex;
+
+        return $this;
+    }
+
+    public function trackTotalHits(bool $value = true): static
+    {
+        $this->trackTotalHits = $value;
 
         return $this;
     }
